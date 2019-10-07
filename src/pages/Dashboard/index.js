353bb60080b315992bb54eae6e2f1e155ce0a561
André Corrêa <andre.murillo.correa@gmail.com>
2019-10-06 23:47:21 -0300

    include push notifications via socket

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import api from '../../services/api';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
+import api from '../../services/api';
 
 import './styles.css';
 
 export default function Dashboard(){
   // vou iniciar com um array vazio porque a função de busca no banco sempre retorna um array
   const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
+
+  const user_id = localStorage.getItem('user');
+  const socket = useMemo(() => socketio('http://localhost:3333', {
+      query: { user_id }
+    }),[user_id]); // vai armazenar a conexão com o socket até que o user_id mude
+
+  useEffect(() => {
+    socket.on('booking_request', data => {
+      setRequests([...requests, data]);
+    })
+  }, [requests, socket]);
 
   // vou chamar com um filtro de lista vazia para o react chamar essa função uma unica vez
   // e não toda vez que algum elemento desse filtro mudar
@@ -23,9 +36,33 @@ export default function Dashboard(){
     loadSpots();   
     
   }, []);
+
+  async function handleAccept(id) {
+    await api.post(`/bookings/${id}/approvals`);
+
+    setRequests(requests.filter(request => request._id !== id));
+  }
+
+  async function handleReject(id) {
+    await api.post(`/bookings/${id}/rejections`);
+
+    setRequests(requests.filter(request => request._id !== id));
+  }
   
   return (
     <>
+
+      <ul className="notifications">
+        {requests.map(request => (
+          <li key={request._id}>
+            <p>
+              <strong>{request.user.email}</strong> está solicitando uma reserva em <strong>{request.spot.company}</strong> para a data: <strong>{request.date}</strong>
+            </p>
+            <button className="accept" onClick={() => handleAccept(request._id)}>ACEITAR</button>
+            <button className="reject" onClick={() => handleReject(request._id)}>REJEITAR</button>
+          </li>
+        ))}
+      </ul>
       <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
